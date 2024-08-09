abacedffc005173e92ae1a92c4013d8bb37f155a
shemchuk <shemchukaga@gmail.com>
2024-08-09 10:31:52 +0200

    adeds new direcnjuies

--- src/test/java/de/telran/urlshortener/UrlShortenerApplicationTests.java
+++ src/test/java/de/telran/urlshortener/UrlShortenerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UrlShortenerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

1a1fcd38a6edf34f0de9907f31789e634b8d0774
shemchuk <shemchukaga@gmail.com>
2024-08-25 08:47:22 +0200

    removeDB

--- src/main/java/de/telran/urlshortener/config/SecurityConfig.java
+++ src/main/java/de/telran/urlshortener/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package de.telran.urlshortener.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,21 +7,28 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Отключение CSRF защиты
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()) // Разрешение доступа ко всем запросам без аутентификации
-                .httpBasic(withDefaults()); // Оставляем HTTP Basic аутентификацию, если она используется
+                .csrf(csrf -> csrf.disable()) // Отключаем защиту от CSRF для упрощения тестирования
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/h2-console/**").permitAll() // Разрешаем доступ к H2 консоли
+                                .requestMatchers("/public/**").permitAll() // Разрешаем доступ к публичным URL
+                                .anyRequest().permitAll() // Разрешаем доступ ко всем остальным URL
+                )
+                .logout(logout ->
+                        logout.permitAll() // Разрешаем доступ к logout
+                )
+                .headers(headers -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)) // Разрешаем использование фреймов
+                );
 
         return http.build();
     }
@@ -32,5 +38,42 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 }
+
+
+// версия с секюрити
+//package de.telran.urlshortener.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+//
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(csrf -> csrf.disable()) // Отключение CSRF защиты
+//                .authorizeHttpRequests(auth -> auth
+//                        .anyRequest().permitAll()) // Разрешение доступа ко всем запросам без аутентификации
+//                .httpBasic(withDefaults()); // Оставляем HTTP Basic аутентификацию, если она используется
+//
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//}
 
 

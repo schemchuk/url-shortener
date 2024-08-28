5cb61c9d7634f45fe6462c919b2b6cae88a08d9c
shemchuk <shemchukaga@gmail.com>
2024-08-28 19:56:05 +0200

    make documentation in the swagger

--- src/main/java/de/telran/urlshortener/controller/RedirectController.java
+++ src/main/java/de/telran/urlshortener/controller/RedirectController.java
@@ -1,6 +1,8 @@
 package de.telran.urlshortener.controller;
 
 import de.telran.urlshortener.service.ShortUrlService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +13,12 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 @RequestMapping("/r")
 @RequiredArgsConstructor
+@Tag(name = "Redirect Controller", description = "Handles URL redirections")
 public class RedirectController {
 
     private final ShortUrlService shortUrlService;
 
+    @Operation(summary = "Redirect to full URL", description = "Redirects to the full URL based on the short key")
     @GetMapping("/{shortKey}")
     public RedirectView redirectToFullUrl(@PathVariable String shortKey) {
         String fullUrl = shortUrlService.getFullUrlByKey(shortKey);

3b409cd9bf0fae2c202ea7d11621bc48661f213a
shemchuk <shemchukaga@gmail.com>
2024-09-06 14:35:22 +0200

    wrotte tests for ShortUrlTrackingService

---
+++ src/test/java/de/telran/urlshortener/service/RedirectServiceTest.java
@@ -0,0 +1,12 @@
+package de.telran.urlshortener.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RedirectServiceTest {
+
+    @Test
+    void getAndTrackFullUrl() {
+    }
+}

91ad24ab68617d09101118c6c7543a9274aa4680
shemchuk <shemchukaga@gmail.com>
2024-09-11 12:35:19 +0200

    corrected dev.properties

--- src/main/java/de/telran/urlshortener/exception/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/de/telran/urlshortener/exception/exceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
-package de.telran.urlshortener.exception;
+package de.telran.urlshortener.exception.exceptionHandler;
 
+import de.telran.urlshortener.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
